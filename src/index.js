b329bde1eb2be5aae32b9ee27875e157e28d7935
IhorLyskov <99328897+IhorLyskov@users.noreply.github.com>
2023-06-02 22:19:27 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 import './styles.css';
 
 import App from './App';
@@ -8,9 +8,9 @@ import App from './App';
 const root = createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
-    <BrowserRouter>
-      {/* <BrowserRouter basename="/crosses-nulls/"> */}
-      <App />
-    </BrowserRouter>
+    {/* <BrowserRouter> */}
+    {/* <BrowserRouter basename="/crosses-nulls/"> */}
+    <App />
+    {/* </BrowserRouter> */}
   </StrictMode>
 );
