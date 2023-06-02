136aa2b16789996a64df062fde601252d936b897
IhorLyskov <99328897+IhorLyskov@users.noreply.github.com>
2023-06-02 21:21:16 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './styles.css';
 
 import App from './App';
@@ -7,13 +8,9 @@ import App from './App';
 const root = createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/crosses-nulls/">
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
 
-// "eslint": "7.19.0",
-// "eslint-config-prettier": "^8.3.0",
-// "eslint-config-standard": "16.0.3",
-// "eslint-plugin-import": "2.25.3",
-// "eslint-plugin-node": "^11.1.0",
-// "eslint-plugin-promise": "5.2.0",
